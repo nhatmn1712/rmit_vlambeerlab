b0676ff3d74923c63a9b00ee64b7d63649f1aa12
nhatmn1712 <s3930338@rmit.edu.vn>
2024-12-11 14:56:03 +0700

    Island Good

--- Assets/Another way to do from yotube/WorldGenerator.cs
+++ Assets/Another way to do from yotube/WorldGenerator.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 
-
 public class WorldGenerator : MonoBehaviour
 {
     public float waterLevel = .4f;
+    public GameObject[] treePreFabs;
     public float scale = .1f;
+    public float treeNoiseScale = .05f;
+    public float treeDesity = .5f;
     public int size = 200;
 
     Cell[,] grid;
 
     void Start()
     {
+        grid = new Cell[size, size];
         float[,] noiseMap = new float[size, size];
         (float xOffset, float yOffset) = (Random.Range(-10000f, 10000f), Random.Range(-10000f, 10000f));
+
+
         for (int y = 0; y < size; y++)
         {
             for (int x = 0; x < size; x++)
@@ -22,7 +27,8 @@ public class WorldGenerator : MonoBehaviour
             }
         }
 
-        float[,] faloffMap = new float[size, size];
+
+        float[,] falloffMap = new float[size, size];
         for (int y = 0; y < size; y++)
         {
             for (int x = 0; x < size; x++)
@@ -30,17 +36,16 @@ public class WorldGenerator : MonoBehaviour
                 float xv = x / (float)size * 2 - 1;
                 float yv = y / (float)size * 2 - 1;
                 float v = Mathf.Max(Mathf.Abs(xv), Mathf.Abs(yv));
-                faloffMap[x, y] = Mathf.Pow(v, 3f) / (Mathf.Pow(v, 3f) + Mathf.Pow(2.2f - 2.2f * v, 3f));
+                falloffMap[x, y] = Mathf.Pow(v, 3f) / (Mathf.Pow(v, 3f) + Mathf.Pow(2.2f - 2.2f * v, 3f));
             }
         }
 
-        grid = new Cell[size, size];
+
         for (int y = 0; y < size; y++)
         {
             for (int x = 0; x < size; x++)
             {
-                float noiseValue = noiseMap[x, y];
-                noiseValue -= faloffMap[x, y];
+                float noiseValue = noiseMap[x, y] - falloffMap[x, y];
                 bool isWater = noiseValue < waterLevel;
 
                 Cell cell = new Cell(isWater);
@@ -51,13 +56,44 @@ public class WorldGenerator : MonoBehaviour
                 cube.transform.localScale = Vector3.one;
 
                 Renderer renderer = cube.GetComponent<Renderer>();
-                if (cell.isWater)
+                renderer.material.color = cell.isWater ? Color.blue : Color.green;
+            }
+        }
+
+
+        GenerateTrees(grid);
+    }
+
+    void GenerateTrees(Cell[,] grid)
+    {
+        float[,] noiseMap = new float[size, size];
+        (float xOffset, float yOffset) = (Random.Range(-10000f, 10000f), Random.Range(-10000f, 10000f));
+
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                float noiseValue = Mathf.PerlinNoise(x * treeNoiseScale + xOffset, y * treeNoiseScale + yOffset);
+                noiseMap[x, y] = noiseValue;
+            }
+        }
+
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Cell cell = grid[x, y];
+                if (!cell.isWater)
                 {
-                    renderer.material.color = Color.blue;
-                }
-                else
-                {
-                    renderer.material.color = Color.green;
+                    float v = Random.Range(0f, treeDesity);
+                    if (noiseMap[x, y] < v)
+                    {
+                        GameObject prefab = treePreFabs[Random.Range(0, treePreFabs.Length)];
+                        GameObject tree = Instantiate(prefab, transform);
+                        tree.transform.position = new Vector3(x, 0, y);
+                        tree.transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+                        tree.transform.localScale = Vector3.one * Random.Range(.8f, 1.2f);
+                    }
                 }
             }
         }
